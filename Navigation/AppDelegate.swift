30008c9d0ecfb9529a1a25a660aba1d72ff11034
georgiy.bondarenko <gbondarenko@rbc.ru>
2023-01-08 13:54:27 +0300

    Refactor realm migration and add encryption

--- Navigation/AppDelegate.swift
+++ Navigation/AppDelegate.swift
@@ -10,7 +10,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        self.realmMigrate()
         FirebaseApp.configure()
         let url = AppConfiguration.allCases.randomElement()!.description
         NetworkService.run(url: url, query: "") { data, response, error in
@@ -22,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 if let responseData = String(data: data, encoding: .utf8) {
                 }
             }
-            
+
         }
         return true
     }
@@ -46,12 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let checkerService = CheckerService()
         checkerService.logout()
         print("logged out in applicationWillTerminate")
-    }
-    
-    private func realmMigrate() -> Void {
-        var config = Realm.Configuration()
-        config.deleteRealmIfMigrationNeeded = true
-        Realm.Configuration.defaultConfiguration = config
     }
 }
 
--- Navigation/Profile/RealmAuthUserStorage.swift
+++ Navigation/Profile/RealmAuthUserStorage.swift
@@ -2,17 +2,26 @@ import Foundation
 import RealmSwift
 
 class RealmAuthUserStorage: AuthUserStorageProtocol {
-    private let realm = try? Realm()
+    private let realm: Realm?
 
+    public init() {
+        let realmService = RealmService()
+        self.realm = realmService.getRealm()
+    }
     func save(_ storedAuthUser: StoredAuthUserProtocol) {
         let authUserToStore = RealmStoredAuthUser(login: storedAuthUser.login, password: storedAuthUser.password)
         realm?.beginWrite()
         realm?.add(authUserToStore, update: .modified)
-        try? realm?.commitWrite()
+        do {
+            try realm?.commitWrite()
+        } catch {
+            print("Unexpected error: \(error).")
+        }
     }
     
     func getLastAuthorized() -> StoredAuthUserProtocol? {
         let lastLoggedIn = realm?.objects(RealmStoredAuthUser.self).sorted(byKeyPath: "lastLoggedInAt", ascending: false).first
+        print(lastLoggedIn)
         return lastLoggedIn
     }
     
