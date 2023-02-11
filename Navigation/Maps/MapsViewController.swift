7ff0fdaf4a9c7e498c98cc7ed2fdce392d211e71
georgiy.bondarenko <gbondarenko@rbc.ru>
2023-02-11 15:55:25 +0300

    Small fixes

--- Navigation/Maps/MapsViewController.swift
+++ Navigation/Maps/MapsViewController.swift
@@ -25,7 +25,7 @@ class MapsViewController: UIViewController, MKMapViewDelegate {
         let pointsOfInterestFilter = MKPointOfInterestFilter()
         mapView.pointOfInterestFilter = pointsOfInterestFilter
         
-        // Москва
+        // Moscow
         let initialLocation = CLLocationCoordinate2D(
             latitude: 55.75222,
             longitude: 37.61556
@@ -50,7 +50,7 @@ class MapsViewController: UIViewController, MKMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Работа с картой"
+        title = "Maps"
         view.backgroundColor = .systemBackground
         
         setupSubviews()
@@ -111,35 +111,26 @@ class MapsViewController: UIViewController, MKMapViewDelegate {
     }
     
     func showRouteOnMap(pickupCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D) {
-
             let request = MKDirections.Request()
             request.source = MKMapItem(placemark: MKPlacemark(coordinate: pickupCoordinate, addressDictionary: nil))
             request.destination = MKMapItem(placemark: MKPlacemark(coordinate: destinationCoordinate, addressDictionary: nil))
             request.requestsAlternateRoutes = true
             request.transportType = .automobile
-
             let directions = MKDirections(request: request)
 
             directions.calculate { [unowned self] response, error in
                 guard let unwrappedResponse = response else { return }
                 
-                //for getting just one route
                 if let route = unwrappedResponse.routes.first {
-                    //show on map
                     self.mapView.addOverlay(route.polyline)
-                    //set the map area to show the route
                     self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets.init(top: 80.0, left: 20.0, bottom: 100.0, right: 20.0), animated: true)
                 }
-
-                //if you want to show multiple routes then you can get all routes in a loop in the following statement
-                //for route in unwrappedResponse.routes {}
             }
         }
-    //this delegate function is for displaying the route overlay and styling it
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
          let renderer = MKPolylineRenderer(overlay: overlay)
-         renderer.strokeColor = UIColor.red
-         renderer.lineWidth = 5.0
+         renderer.strokeColor = UIColor.blue
+         renderer.lineWidth = 4.0
          return renderer
     }
 }
