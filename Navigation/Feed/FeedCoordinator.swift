8ea3a2a1b10192fcdfeaf5ca4fd8b2a3a9a56b26
georgiy.bondarenko <gbondarenko@rbc.ru>
2023-03-26 12:10:56 +0300

    Remove useless

--- Navigation/Feed/FeedCoordinator.swift
+++ Navigation/Feed/FeedCoordinator.swift
@@ -17,17 +17,4 @@ final class FeedCoordinator: Coordinatable {
     func openPost(post: PostAggregate) {
         navigationController.pushViewController(PostAggregateViewController(post: post), animated: true)
     }
-    
-    func showAlert() {
-        let alert = UIAlertController(title: String(localized: "My Alert"), message: String(localized: "This is an alert."), preferredStyle: .alert)
-        let okAction = UIAlertAction(title: String(localized: "OK"), style: .default) {
-            UIAlertAction in
-        }
-        alert.addAction(okAction)
-        let cancelAction = UIAlertAction(title: String(localized: "Cancel"), style: .default) {
-            UIAlertAction in
-        }
-        alert.addAction(cancelAction)
-        navigationController.present(alert, animated: true, completion: nil)
-    }
 }
