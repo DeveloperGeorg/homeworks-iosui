51fc244ed4709a24a6743a02b1f426fdb3badcac
georgiy.bondarenko <gbondarenko@rbc.ru>
2023-03-28 09:39:06 +0300

    Create form with text field and button

--- Navigation/Posts/CreatePostViewController.swift
+++ Navigation/Posts/CreatePostViewController.swift
@@ -4,6 +4,42 @@ import Uploadcare
 class CreatePostViewController: UIViewController {
     private let postItemDataStorage: PostItemDataStorageProtocol
     private let fileUploader: FileUploaderProtocol
+    
+    var contentTextField: UITextView = {
+        let textField = UITextView()
+        textField.isEditable = true
+        textField.isScrollEnabled = true
+        textField.backgroundColor = UiKitFacade.shared.getSecondaryBackgroundColor()
+        textField.layer.cornerRadius = 12
+        textField.font = UiKitFacade.shared.getRegularTextFont()
+        textField.textColor = UiKitFacade.shared.getPrimaryTextColor()
+        textField.layer.borderColor = UiKitFacade.shared.getAccentColor().cgColor
+        textField.layer.borderWidth = 1
+        
+//        let paddingView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 40))
+//        textField.leftView = paddingView
+//        textField.leftViewMode = .always
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
+    var createPostButton: CustomButton = {
+        let button = CustomButton(
+            title: String(localized: "Create the post"),
+            titleColor: UiKitFacade.shared.getPrimaryTextColor(),
+            titleFor: .normal,
+            buttonTappedCallback: nil
+        )
+        button.layer.cornerRadius = 4
+        button.backgroundColor = UiKitFacade.shared.getAccentColor()
+        button.layer.shadowOpacity = 0.7
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowColor = UiKitFacade.shared.getAccentColor().cgColor
+        button.layer.shadowRadius = CGFloat(4)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
 
     init() {
         self.postItemDataStorage = FirestorePostItemDataStorage()
@@ -21,22 +57,38 @@ class CreatePostViewController: UIViewController {
         super.viewDidLoad()
         self.title = "Create new post"
         view.backgroundColor = UiKitFacade.shared.getPrimaryBackgroundColor()
-        self.fileUploader.uploadFile() {fileName, errorMessage in
-            DispatchQueue.main.async {
-                print("filename: \(fileName)")
-                if let fileName = fileName {
-                    print("start creating post")
-                    let post = PostItem(
-                        blogger: "TIQqWZVxbn03MRxsioz6",
-                        mainImageLink: fileName,
-                        content: "generated content \(UUID.init().uuidString)"
-                    )
-                    self.postItemDataStorage.create(post) { posts, hasMore in
-                        print("post item")
+        view.addSubview(contentTextField)
+        view.addSubview(createPostButton)
+        initConstraints()
+        createPostButton.setButtonTappedCallback({sender in
+            let content = self.contentTextField.text ?? ""
+            self.fileUploader.uploadFile() {fileName, errorMessage in
+                DispatchQueue.main.async {
+                    if let fileName = fileName {
+                        let post = PostItem(
+                            blogger: "TIQqWZVxbn03MRxsioz6",
+                            mainImageLink: fileName,
+                            content: content
+                        )
+                        self.postItemDataStorage.create(post) { posts, hasMore in
+                            print("post item was created")
+                        }
                     }
                 }
             }
-        }
+        })
     }
 
+    private func initConstraints() {
+        NSLayoutConstraint.activate([
+            contentTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            contentTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            contentTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            contentTextField.heightAnchor.constraint(equalToConstant: 500),
+            createPostButton.topAnchor.constraint(equalTo: contentTextField.bottomAnchor, constant: 8),
+            createPostButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            createPostButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            createPostButton.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
 }
